a74888c2d75edee456f91d71d8f742b400ee3233
983151 <983151@CSLAB-M105-309>
2016-08-17 16:45:21 -0500

    Committed Hadoop Project

---
+++ Hadoop/MapReduce.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Hadoop.MapReduce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Hadoop
+{
+    //Mapper
+    public class NamespaceMapper : MapperBase
+    {
+        //Override the map method.
+        public override void Map(string inputLine, MapperContext context)
+        {
+            //Extract the words from the text files
+
+            var words = Regex.Split(inputLine, @"\W");
+
+            foreach (string word in words)
+            {
+                //Just emit the words.
+                context.EmitKeyValue(word, "1");
+            }
+        }
+    }
+
+    //Reducer
+    public class NamespaceReducer : ReducerCombinerBase
+    {
+        //Accepts each key and count the occurrances
+        public override void Reduce(string key, IEnumerable<string> values,
+            ReducerCombinerContext context)
+        {
+            //Write back  
+            context.EmitKeyValue(key, values.Count().ToString());
+        }
+    }
+
+    //Our Namespace counter job
+    public class NamespaceCounterJob : HadoopJob<NamespaceMapper, NamespaceReducer>
+    {
+        public override HadoopJobConfiguration Configure(ExecutorContext context)
+        {
+            var config = new HadoopJobConfiguration();
+            config.InputPath = "/user/input/TextFiles";
+            config.OutputFolder = "/user/output/TextFiles";
+            return config;
+        }
+    }
+}
--- Hadoop/Program.cs
+++ Hadoop/Program.cs
@@ -10,6 +10,8 @@ namespace Hadoop
     {
         static void Main(string[] args)
         {
+            var hadoop = Microsoft.Hadoop.MapReduce.Hadoop.Connect();
+            var result = hadoop.MapReduceJob.ExecuteJob<NamespaceCounterJob>();
         }
     }
 }
